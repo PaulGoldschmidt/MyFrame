b259f7454923c284e9033050f73aa5c8ba81222f
Paul Goldschmidt <github@goldschmidt.id>
2023-11-29 22:11:29 +0100

    reorganisation

--- Software/MyFrame/MyFrame.ino
+++ Software/MyFrame/MyFrame.ino
@@ -1,7 +1,7 @@
 /*
  * MyFrame Version 1
  */
-
+ 
 #include "wifi_info.h"
 #include <Arduino.h>
 #include <arduino_homekit_server.h>
@@ -95,75 +95,6 @@ void my_homekit_loop() {
     next_heap_millis = t + 5 * 1000;
     LOG_D("Free heap: %d, HomeKit clients: %d",
           ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
-  }
-}
-
-void set_on(const homekit_value_t v) {
-  bool on = v.bool_value;
-  cha_on.value.bool_value = on;  //sync the value
-
-  if (on) {
-    is_on = true;
-    Serial.println("On");
-  } else {
-    is_on = false;
-    Serial.println("Off");
-  }
-  setLEDs(current_temp, current_brightness);
-}
-
-void set_temp(const homekit_value_t v) {
-  Serial.print("set_temp, value:");
-  int temp = v.int_value;
-  cha_color_temperature.value.int_value = temp;  //sync the value
-
-  current_temp = temp;
-  Serial.println(current_temp);
-  setLEDs(current_temp, current_brightness);
-}
-
-void set_bright(const homekit_value_t v) {
-  Serial.println("set_bright, value:");
-  int bright = v.int_value;
-  cha_bright.value.int_value = bright;  //sync the value
-  bright = map(bright, 0, 100, 0, 255);
-  current_brightness = bright;
-  Serial.println(current_brightness);
-  setLEDs(current_temp, current_brightness);
-}
-
-void setLEDs(int mired, int brightness) {
-  if (is_on == true) {
-    int warmIntensity = map(mired, 50, 400, 0, brightness);  // Map the warm intensity for the LED controls
-    int coldIntensity = map(mired, 50, 400, brightness, 0);
-    warmIntensity = (warmIntensity < 0) ? 0 : warmIntensity;  // If variable is smaller then zero, set to zero
-    coldIntensity = (coldIntensity < 0) ? 0 : coldIntensity;  // we don't use an unsigned int because of oferv
-
-    Serial.print("Warm Value: ");
-    Serial.print(warmIntensity);
-    Serial.print(" | Cold Value: ");
-    Serial.println(coldIntensity);
-    analogWrite(warmPin, warmIntensity);
-    analogWrite(coldPin, coldIntensity);
-    // Write to EEPROM
-    EEPROM.begin(512);
-    EEPROM.put(addrWarmIntensity, warmIntensity);
-    EEPROM.put(addrColdIntensity, coldIntensity);
-    EEPROM.commit();
-    EEPROM.end();
-    Serial.print("Written to EEPROM - Warm intensity: ");
-    Serial.print(warmIntensity);
-    Serial.print(" | Cold intensity: ");
-    Serial.println(coldIntensity);
-  } else {
-    analogWrite(warmPin, 0);
-    analogWrite(coldPin, 0);
-    // Write to EEPROM
-    EEPROM.begin(512);
-    EEPROM.put(addrWarmIntensity, 0);
-    EEPROM.put(addrColdIntensity, 0);
-    EEPROM.commit();
-    EEPROM.end();
   }
 }
 
---
+++ software/MyFrame/led-controller.ino
@@ -0,0 +1,68 @@
+void set_on(const homekit_value_t v) {
+  bool on = v.bool_value;
+  cha_on.value.bool_value = on;  //sync the value
+
+  if (on) {
+    is_on = true;
+    Serial.println("On");
+  } else {
+    is_on = false;
+    Serial.println("Off");
+  }
+  setLEDs(current_temp, current_brightness);
+}
+
+void set_temp(const homekit_value_t v) {
+  Serial.print("set_temp, value:");
+  int temp = v.int_value;
+  cha_color_temperature.value.int_value = temp;  //sync the value
+
+  current_temp = temp;
+  Serial.println(current_temp);
+  setLEDs(current_temp, current_brightness);
+}
+
+void set_bright(const homekit_value_t v) {
+  Serial.println("set_bright, value:");
+  int bright = v.int_value;
+  cha_bright.value.int_value = bright;  //sync the value
+  bright = map(bright, 0, 100, 0, 255);
+  current_brightness = bright;
+  Serial.println(current_brightness);
+  setLEDs(current_temp, current_brightness);
+}
+
+void setLEDs(int mired, int brightness) {
+  if (is_on == true) {
+    int warmIntensity = map(mired, 50, 400, 0, brightness);  // Map the warm intensity for the LED controls
+    int coldIntensity = map(mired, 50, 400, brightness, 0);
+    warmIntensity = (warmIntensity < 0) ? 0 : warmIntensity;  // If variable is smaller then zero, set to zero
+    coldIntensity = (coldIntensity < 0) ? 0 : coldIntensity;  // we don't use an unsigned int because of oferv
+
+    Serial.print("Warm Value: ");
+    Serial.print(warmIntensity);
+    Serial.print(" | Cold Value: ");
+    Serial.println(coldIntensity);
+    analogWrite(warmPin, warmIntensity);
+    analogWrite(coldPin, coldIntensity);
+    // Write to EEPROM
+    EEPROM.begin(512);
+    EEPROM.put(addrWarmIntensity, warmIntensity);
+    EEPROM.put(addrColdIntensity, coldIntensity);
+    EEPROM.commit();
+    EEPROM.end();
+    Serial.print("Written to EEPROM - Warm intensity: ");
+    Serial.print(warmIntensity);
+    Serial.print(" | Cold intensity: ");
+    Serial.println(coldIntensity);
+  } else {
+    analogWrite(warmPin, 0);
+    analogWrite(coldPin, 0);
+    // Write to EEPROM
+    EEPROM.begin(512);
+    EEPROM.put(addrWarmIntensity, 0);
+    EEPROM.put(addrColdIntensity, 0);
+    EEPROM.commit();
+    EEPROM.end();
+  }
+}
